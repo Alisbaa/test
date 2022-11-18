96b5424e1c851d22d601b5840fd98a0cf02ee4d3
Alisa <alisaasila2004@gmail.com>
2022-11-19 04:35:56 +0600

    first commit

---
+++ my/my/my.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <list>     
+#include <iterator> 
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    list<int> myList;
+    for (int i = 0; i < 15; i++)
+    {
+        myList.push_back(rand() % 20);
+    }
+
+    cout << "List: ";
+    for (list<int>::iterator it = myList.begin(); it != myList.end(); it++)
+    {
+        cout << *it << " ";
+    }
+        int max = myList.front();
+        for (auto l : myList)
+            if (max < l) max = l;
+        cout << "max: " << max << endl;
+    
+}
